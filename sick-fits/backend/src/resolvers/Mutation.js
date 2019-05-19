26af730ccd5d7409a54f17ee5f1bb0a8c42bdfe0
Lailz <lailaabdulraheem@gmail.com>
2019-05-19 03:38:37 +0300

    cleaned up the render prop mess

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -196,6 +196,61 @@ const Mutations = {
       },
       info
     );
+  },
+  async addToCart(parent, args, ctx, info) {
+    const { userId } = ctx.request;
+    if (!userId) {
+      throw new Error("You must beloggen in hon");
+    }
+    const [existingCartItem] = await ctx.db.query.cartItems({
+      where: {
+        user: { id: userId },
+        item: { id: args.id }
+      }
+    });
+    if (existingCartItem) {
+      console.log("This item is already in the cart");
+      return ctx.db.mutation.updateCartItem(
+        {
+          where: { id: existingCartItem.id },
+          data: { quantity: existingCartItem.quantity + 1 }
+        },
+        info
+      );
+    }
+    return ctx.db.mutation.createCartItem(
+      {
+        data: {
+          user: {
+            connect: { id: userId }
+          },
+          item: {
+            connect: { id: args.id }
+          }
+        }
+      },
+      info
+    );
+  },
+  async removeFromCart(parent, args, ctx, info) {
+    const cartItem = await ctx.db.query.cartItem(
+      {
+        where: {
+          id: args.id
+        }
+      },
+      `{id, user {id}}`
+    );
+    if (!cartItem) throw new Error("No CartItem Found!");
+    if (cartItem.user.id !== ctx.request.userId) {
+      throw new Error("No cheating");
+    }
+    return ctx.db.mutation.deleteCartItem(
+      {
+        where: { id: args.id }
+      },
+      info
+    );
   }
 };
 
--- sick-fits/frontend/components/Item.js
+++ sick-fits/frontend/components/Item.js
@@ -7,6 +7,7 @@ import ItemStyles from "./styles/ItemStyles";
 import PriceTag from "./styles/PriceTag";
 import formatMoney from "../lib/formatMoney";
 import DeleteItem from "./DeleteItem";
+import AddToCart from "./AddToCart";
 
 class Item extends Component {
   static propTypes = {
@@ -28,7 +29,7 @@ class Item extends Component {
           <Link href={{ pathname: "update", query: { id: item.id } }}>
             <a>Edit</a>
           </Link>
-          <button>Add to Cart</button>
+          <AddToCart id={item.id} />
           <DeleteItem id={item.id}>Delete this item</DeleteItem>
         </div>
       </ItemStyles>
